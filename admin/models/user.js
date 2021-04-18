ad0383364549406828f6b727fbce344042bf7acb
rhs99 <1605010@ugrad.cse.buet.ac.bd>
2021-04-18 21:09:11 +0600

    create user with password hashing enabled

--- admin/models/user.js
+++ admin/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
@@ -38,13 +39,28 @@ const userSchema = new mongoose.Schema({
         }
     }]
 }, {
-    timestamps: true
+    timestamps: true,
+    strict: false
 })
 
 userSchema.methods.generateAuthToken = async function(){
-    return jwt.sign({_id: this._id.toString()},
-        "uiisSecret")
+    const token = jwt.sign({
+        _id: this._id.toString()
+    }, process.env.JSON_WEB_TOKEN)
+
+    this.tokens = this.tokens.concat({ token })
+    await this.save()
+
+    return token
 }
+
+//hash the plain text password before saving
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 8)
+    }
+    next()
+})
 
 const User = mongoose.model('User', userSchema)
 
--- admin/routers/user.js
+++ admin/routers/user.js
@@ -6,13 +6,12 @@ const router = new express.Router()
 router.post('/create-account', async (req, res) => {
     const user = new User(req.body)
     try {
+        user.set('testID',"test1")
         await user.save()
+        console.log(user)
         const token = await user.generateAuthToken()
-        user.tokens = user.tokens.concat({token})
-        await user.save()
-
-        res.status(201).send({token})
-    } catch (e){
+        res.status (201).send({token})
+    } catch (error) {
         res.status(400).send({
             error: e.message
         })

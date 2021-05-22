f96f107c74e3544f01e2a39d6fe216f29d404d0d
FromSaffroncity <yasar199510@gmail.com>
2021-05-22 22:10:02 +0600

    advisees get api added

--- src/teacher/advisor/router.js
+++ src/teacher/advisor/router.js
@@ -1,4 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const { Student } = require('../../admin/accounts/model');
+
 const router =  express.Router();
+
+router.get('/teacher/advisor/advisees', async (req, res) => {
+    try {
+        const advisees = await Student
+            .find({ 'advisor': req.user._id })
+            .select('_id name email contactNumber residentialAddress department hall level term');
+        res.status(200).json(advisees);
+    } catch(error) {
+        res.status(400).send({
+            error: error.message
+        });
+    }
+});
 
 module.exports = router;

9dd452b63e5284d7a36201e56d3ec7a754b6bd3c
FromSaffroncity <yasar199510@gmail.com>
2021-06-10 14:52:46 +0600

    student side updated

--- src/student/basic/router.js
+++ src/student/basic/router.js
@@ -1,5 +1,39 @@
 const express = require('express');
 
+const { Student } = require('../../admin/accounts/model');
+
 const router =  express.Router();
+
+router.get('/basic', async (req, res) => {
+    try {
+        const student = await Student
+            .findById({
+                _id: req.user._id
+            })
+            .select('_id name level term department hall');
+
+        res.status(200).send(student);
+    } catch(error) {
+        res.status(400).send({
+            error: error.message
+        });
+    }
+});
+
+router.get('/details', async (req, res) => {
+    try {
+        const student = await Student
+            .findById({
+                _id: req.user._id
+            })
+            .select('_id name level term department hall contactNumber email residentialAddress');
+
+        res.status(200).send(student);
+    } catch(error) {
+        res.status(400).send({
+            error: error.message
+        });
+    }
+});
 
 module.exports = router;

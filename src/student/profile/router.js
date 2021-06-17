f526a15436324b44a4c6adaee6f91b3aa0c9836f
FromSaffroncity <yasar199510@gmail.com>
2021-06-17 22:04:42 +0600

    apis updated

--- src/student/profile/router.js
+++ src/student/profile/router.js
@@ -10,38 +10,6 @@ router.get('/id', async (req, res) => {
     });
 });
 
-router.get('/basic/:id', async (req, res) => {
-    try {
-        const student = await Student
-            .findById({
-                _id: req.params.id
-            })
-            .select('_id level term');
-
-        res.status(200).send(student);
-    } catch(error) {
-        res.status(400).send({
-            error: error.message
-        });
-    }
-});
-
-router.get('/home/:id', async (req, res) => {
-    try {
-        const student = await Student
-            .findById({
-                _id: req.params.id
-            })
-            .select('_id name level term department hall');
-
-        res.status(200).send(student);
-    } catch(error) {
-        res.status(400).send({
-            error: error.message
-        });
-    }
-});
-
 router.get('/profile/:id', async (req, res) => {
     try {
         const student = await Student
@@ -49,22 +17,6 @@ router.get('/profile/:id', async (req, res) => {
                 _id: req.params.id
             })
             .select('_id name level term department hall contactNumber email residentialAddress');
-
-        res.status(200).send(student);
-    } catch(error) {
-        res.status(400).send({
-            error: error.message
-        });
-    }
-});
-
-router.get('/grades_profile/:id', async (req, res) => {
-    try {
-        const student = await Student
-            .findById({
-                _id: req.params.id
-            })
-            .select('_id name department totalCreditHoursCompleted cgpa');
 
         res.status(200).send(student);
     } catch(error) {

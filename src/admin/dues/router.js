f4273b29fc8b73c18a302b325d371f57cb2e3ff4
rhs99 <1605010@ugrad.cse.buet.ac.bd>
2021-06-20 15:38:36 +0600

    due router added

--- src/admin/dues/router.js
+++ src/admin/dues/router.js
@@ -11,7 +11,7 @@ const { User, Student } = require('../accounts/model')
 const router = new express.Router()
 
 // dept, hall, level, term, ids
-router.post('/create/levelChangingFee/', async (req, res) => {
+router.post('/upsert/levelChangingFee/', async (req, res) => {
     let match = {}
     const bulkSize = 2
 
@@ -58,7 +58,7 @@ router.post('/create/levelChangingFee/', async (req, res) => {
             }
             mongoDue = new LevelChangingFee({
                 // amount: req.body.amount,
-                amount: (student._id === '1605001' || student._id === '1605003')? undefined: req.body.amount,
+                amount: req.body.amount,
                 issueDate: Date.now(),
                 deadline: req.body.deadline,
                 delayFine: req.body.delayFine,
@@ -71,14 +71,13 @@ router.post('/create/levelChangingFee/', async (req, res) => {
             if (err) {
                 throw new Error(err)
             }
-            console.log(mongoDue)
 
             dues.push(due)
 
             if (dues.length === bulkSize) {
                 it++
                 console.log(`Iteration No ${it}`)
-                const res = await runInTransaction(dues)
+                const res = await runInTransaction(dues, LevelChangingFee)
                 cnt += res.upsertedCount + res.modifiedCount
                 dues = []
             }
@@ -86,7 +85,101 @@ router.post('/create/levelChangingFee/', async (req, res) => {
 
         if (dues.length > 0){
             console.log(`Last iteration!`)
-            const res = await runInTransaction(dues)
+            const res = await runInTransaction(dues, LevelChangingFee)
+            cnt += res.upsertedCount + res.modifiedCount
+            dues = []
+        }
+        totalTime = (Date.now() - startTime)
+        console.log(`Inserted ${cnt} elements in ${totalTime} ms`)
+
+        res.status(201).send({
+            duesModified: cnt
+        })
+
+    } catch (error) {
+        res.status(400).send({
+            error: error.message,
+            duesModified: cnt
+        })
+    }
+})
+ 
+
+// (hall, dept, level, term, ids
+router.post('/upsert/examFee/', async (req, res) => {
+    let match = {}
+    const bulkSize = 2
+
+    if (req.body.department){
+        match.department = req.body.department
+    }
+    if (req.body.hall){
+        match.hall = req.body.hall
+    }
+    if (req.body.term){
+        match.term = req.body.term
+    }
+    if (req.body.level){
+        match.level = req.body.level
+    }
+    if (req.body.ids){
+        match._id = {
+            $in: req.body.ids
+        }
+    }
+    let cnt = 0, it = 0, totalTime
+    try {
+        let dues = []
+        const startTime = Date.now()
+        const cursor = await Student.find(match).cursor()
+        let mongoDue = undefined
+
+        await cursor.eachAsync(async function (student) {
+            const due = {
+                updateOne: {
+                    filter: {issuedTo: student._id, session: new Date(req.body.session)} ,
+                    update: {
+                        amount: req.body.amount,
+                        issueDate: Date.now(),
+                        deadline: req.body.deadline,
+                        delayFine: req.body.delayFine,
+                        issuedTo: student.id,
+                        level: student.level,
+                        term: student.term,
+                        session: req.body.session
+                    },
+                    upsert: true
+                }
+            }
+            mongoDue = new ExamFee({
+                // amount: req.body.amount,
+                amount: req.body.amount,
+                issueDate: Date.now(),
+                deadline: req.body.deadline,
+                delayFine: req.body.delayFine,
+                issuedTo: student.id,
+                level: student.level,
+                term: student.term,
+                session: req.body.session
+            })
+            const err = mongoDue.validateSync()
+            if (err) {
+                throw new Error(err)
+            }
+            dues.push(due)
+
+            if (dues.length === bulkSize) {
+                it++
+                console.log(`Iteration No ${it}`)
+                const res = await runInTransaction(dues, ExamFee)
+                cnt += res.upsertedCount + res.modifiedCount
+                dues = []
+            }
+        })
+
+        if (dues.length > 0){
+            console.log(`Last iteration!`)
+            const res = await runInTransaction(dues, ExamFee)
             cnt += res.upsertedCount + res.modifiedCount
             dues = []
         }
@@ -105,11 +198,101 @@ router.post('/create/levelChangingFee/', async (req, res) => {
     }
 })
 
-const runInTransaction = async function(dues) {
+ 
+// (hall, level, term, ids)
+router.post('/upsert/diningFee/', async (req, res) => {
+    let match = {}
+    const bulkSize = 2
+
+    if (req.body.hall){
+        match.hall = req.body.hall
+    }
+    if (req.body.term){
+        match.term = req.body.term
+    }
+    if (req.body.level){
+        match.level = req.body.level
+    }
+    if (req.body.ids){
+        match._id = {
+            $in: req.body.ids
+        }
+    }
+    let cnt = 0, it = 0, totalTime
+    try {
+        let dues = []
+        const startTime = Date.now()
+        const cursor = await Student.find(match).cursor()
+        let mongoDue = undefined
+
+        await cursor.eachAsync(async function (student) {
+            const due = {
+                updateOne: {
+                    filter: {issuedTo: student._id, yearMonth: new Date(req.body.yearMonth)} ,
+                    update: {
+                        amount: req.body.amount,
+                        issueDate: Date.now(),
+                        deadline: req.body.deadline,
+                        delayFine: req.body.delayFine,
+                        issuedTo: student.id,
+                        level: student.level,
+                        term: student.term,
+                        yearMonth: req.body.yearMonth
+                    },
+                    upsert: true
+                }
+            }
+            mongoDue = new DiningFee({
+                amount: req.body.amount,
+                issueDate: Date.now(),
+                deadline: req.body.deadline,
+                delayFine: req.body.delayFine,
+                issuedTo: student.id,
+                level: student.level,
+                term: student.term,
+                yearMonth: req.body.yearMonth
+            })
+            const err = mongoDue.validateSync()
+            if (err) {
+                throw new Error(err)
+            }
+            dues.push(due)
+
+            if (dues.length === bulkSize) {
+                it++
+                console.log(`Iteration No ${it}`)
+                const res = await runInTransaction(dues, DiningFee)
+                cnt += res.upsertedCount + res.modifiedCount
+                dues = []
+            }
+        })
+
+        if (dues.length > 0){
+            console.log(`Last iteration!`)
+            const res = await runInTransaction(dues, DiningFee)
+            cnt += res.upsertedCount + res.modifiedCount
+            dues = []
+        }
+        totalTime = (Date.now() - startTime)
+        console.log(`Inserted ${cnt} elements in ${totalTime} ms`)
+
+        res.status(201).send({
+            duesModified: cnt
+        })
+
+    } catch (error) {
+        res.status(400).send({
+            error: error.message,
+            duesModified: cnt
+        })
+    }
+})
+
+const runInTransaction = async function(dues, TargetModel) {
     const session = await mongoose.startSession()
     session.startTransaction()
     try {
-        const res = await LevelChangingFee.bulkWrite(dues, {session})
+        const res = await TargetModel.bulkWrite(dues, {session})
         await session.commitTransaction()
         return res
     } catch (error){
@@ -121,51 +304,8 @@ const runInTransaction = async function(dues) {
 }
 
 
-router.patch('/update/levelChangingFee/', async (req, res) => {
-    try {
-        let match = {}
-        if (req.body.department){
-            match.department = req.body.department
-        }
-        if (req.body.hall){
-            match.hall = req.body.hall
-        }
-        if (req.body.term){
-            match.term = req.body.term
-        }
-        if (req.body.level){
-            match.level = req.body.level
-        }
-        if (req.body.ids){
-            match.issuedTo = {
-                $in: req.body.ids
-            }
-        }
-        if (req.body.session){
-            match.session = req.body.session
-        }
-
-        let updateList = {}
-        if(req.body.amount){
-            updateList.amount = req.body.amount
-        }
-        if(req.body.deadline){
-            updateList.deadline = req.body.deadline
-        }
-        if(req.body.delayFine){
-            updateList.delayFine = req.body.delayFine
-        }
-        await LevelChangingFee.updateMany(match, {
-            $set: updateList
-        })
-         
-        res.status(200).send()
-    } catch (error) {
-        res.status(400).send({
-            error: error.message
-        })
-    }
-})
 
 
+
+ 
 module.exports = router

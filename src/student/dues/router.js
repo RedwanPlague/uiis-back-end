0e0bccc564ccc6bb84e6cccb4d0389286ecb9238
rhs99 <1605010@ugrad.cse.buet.ac.bd>
2021-06-26 12:13:37 +0600

    student due added

---
+++ src/student/dues/router.js
@@ -0,0 +1,33 @@
+const express = require('express')
+
+const constants = require('../../utils/constants')
+const { Due } = require('../../admin/dues/model')
+const { Fine } = require('../../admin/fines/model')
+
+const router =  express.Router()
+
+router.get('/getDue', async (req, res) => {
+    try {
+        if(req.user.userType !== constants.USER_TYPES.STUDENT){
+            throw new Error('Only for students!')
+        }
+        const student = req.user
+
+        const dues = await Due.find({
+            issuedTo: student._id,
+            status: constants.DUE_STATUS.PENDING
+        })
+
+        const fines = await Fine.find({
+            issuedTo: student._id,
+            status: constants.DUE_STATUS.PENDING
+        })
+        res.send({dues,fines})       
+    } catch (error) {
+        res.status(400).send()
+    }
+})
+ 
+
+module.exports = router
+
